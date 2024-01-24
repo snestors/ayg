2ba84ade2ad8f1ec87bf87cf3af790ce69329ed6
Nestor <snestors@gmail.com>
2024-01-24 06:23:03 -0500

    se agrego supa y bootstarp

--- src/components/thermal.jsx
+++ src/components/thermal.jsx
@@ -1,65 +1,100 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
 import Barcode from 'react-barcode';
 import logo from '../assets/ISOTIPO.png'
 
 const ImageGenerator = ({ vin, marca, modelo, color }) => {
   const imageRef = useRef(null);
+  const [base64, setBase64] = useState("")
+
   const BtPrint = (data) => {
+    console.log("brt")
+
     const intentScheme = "#Intent;scheme=rawbt;";
     const packageString = "package=ru.a402d.rawbtprinter;end;";
     const encodedData = encodeURI(data);
-
-    window.location.href = "intent:" + encodedData + intentScheme + packageString;
+    setTimeout(() => {
+      window.location.href = "intent:" + encodedData + intentScheme + packageString;
+      
+    }, 2000);
   };
 
-  const generateImage = () => {
-    html2canvas(imageRef.current).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
+  const generateAndPrintImage = async () => {
+    // Ajusta la escala para mejorar la calidad de la imagen
+    const scale = 2;
 
-      // Puedes guardar la imagen o realizar otras acciones con imgData
+    await html2canvas(imageRef.current, { scale }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      
+      // Llama a BtPrint con la información que deseas imprimir
+      console.log("adsad")
+      BtPrint(imgData);
+      // Puedes guardar la imagen o realizar otras acciones con imgData si es necesario
       console.log(imgData);
+      setBase64(imgData)
+      return imgData;
     });
   };
 
+
+  useEffect(()=>{
+    
+    console.log(base64)
+  })
+
   return (
     <>
-    <div ref={imageRef} style={{
-        position: 'relative',
-        width: '48mm',
-        height: '25mm',
-        border: '1px solid black',
-        padding: '1mm',
-        fontSize: '15px', // Ajusta el tamaño de la fuente según sea necesario
-        lineHeight: '1', // Ajusta el espaciado entre líneass
-        display: 'flex',
+    
+      <div
+        ref={imageRef}
+        style={{
+          position: 'relative',
+          width: '383px',
+          height: '200px',
+          paddingLeft: '50px',
+          paddingTop: '5px',
+          paddingRight: '5px',
+          paddingBottom: '5px',
+          fontSize: '22px',
+          gap:'1px',
+          display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'space-between',
-     }}>
-      <div>
-        <Barcode format='CODE128' margin={0} displayValue={false } value={vin} width={0.9} height={20} />
-        <strong>VIN:</strong> {vin}
-      </div>
-      <div>
-        <strong>MARCA:</strong> {marca}
-      </div>
-      <div>
-        <strong>MODELO:</strong> {modelo}
-      </div>
-      <div>
-        <strong>COLOR:</strong> {color}
-      </div>
-      <div>
-       
-      </div>
-      <img
+        
+        }}
+      >
+        <div>
+          <Barcode format='CODE128' margin={0} displayValue={false} value={vin} width={1.8} height={30} />
+        </div>
+        <div>
+          <strong>VIN:</strong> {vin}
+        </div>
+        <div>
+          <strong>MARCA:</strong> {marca}
+        </div>
+        <div >
+          <strong  style={{
+          maxWidth: '350px',  // Establece el ancho máximo del texto a 350px
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+        }}>MODELO:</strong> {modelo}
+        </div>
+        <div  style={{
+          maxWidth: '350px',  // Establece el ancho máximo del texto a 350px
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+        }}>
+          <strong>COLOR:</strong> {color}
+        </div>
+        <img
         src={logo}
         
         style={{ width: '50px', height: 'auto', position: 'absolute', bottom: '0', right: '0' }}
       />
-    </div>
-    
-      <button onClick={generateImage}>Generar Imagen</button>
+      </div>
+       
+      <button onClick={generateAndPrintImage} className="btn btn-danger m-0 ">Generar Imagen y Imprimir</button>
     </>
   );
 };
---
+++ src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient  } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://vyhwfmileoexngtuuwue.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5aHdmbWlsZW9leG5ndHV1d3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI4MjM1NTAsImV4cCI6MjAwODM5OTU1MH0.zo8mvXHVlMRmem09sX71UGkge8MYmwbqKWTRtua6NxE");
+
